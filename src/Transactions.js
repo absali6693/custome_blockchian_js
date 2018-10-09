18588f4574d760d3392440e7488dd849b72554c2
Abbas Ali <absali6693@gmail.com>
2018-10-09 12:34:32 -0400

    Added Transaction file. This file conatins the stucture for a transactions.

---
+++ src/Transactions.js
@@ -0,0 +1,9 @@
+module.exports = class Transactions{
+    constructor(fromAddress, toAddress, amount) {
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
+    }
+
+    
+}
